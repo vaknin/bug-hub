8ab4f0ff76e49337d86ed917e91e05cd6f833198
vaknin <vaknins33@gmail.com>
2019-07-05 08:55:54 +0300

    Updates

--- precache-manifest.53da8496e997e093c4611a21fced3135.js
+++ precache-manifest.53da8496e997e093c4611a21fced3135.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0986ef93a8fd000dd34e602da4cd40ba",
+    "revision": "bb4f8eb05b68d1b129d32a632ffd588e",
     "url": "/manage-bugs/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/manage-bugs/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "28eb926692bfabeb379f",
+    "revision": "2e5509db23ac298b77c4",
     "url": "/manage-bugs/static/css/main.d2281247.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/manage-bugs/static/js/2.4f9cec50.chunk.js"
   },
   {
-    "revision": "28eb926692bfabeb379f",
-    "url": "/manage-bugs/static/js/main.062054dd.chunk.js"
+    "revision": "2e5509db23ac298b77c4",
+    "url": "/manage-bugs/static/js/main.e39aee13.chunk.js"
   },
   {
     "revision": "7a950a6c58f45ba63c2c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/manage-bugs/precache-manifest.bccf6755a25021be3dd631bbee5ecbe5.js"
+  "/manage-bugs/precache-manifest.53da8496e997e093c4611a21fced3135.js"
 );
 
 self.addEventListener('message', (event) => {
